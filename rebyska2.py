3a011f771ca77d17c8c0ce469eb7f1ab8d457cd4
Jan Frait <fraitjan@fit.cvut.cz>
2020-02-29 22:31:38 +0100

    reby2

--- rebyska2.py
+++ rebyska2.py
@@ -1,18 +1,21 @@
-def vypis_jmena(jmeno, prijmeni):
-    print("Jmenujes se: " + jmeno + " " + prijmeni)
+import random
 
+BLUE = '\033[94m'
+GREEN = '\033[92m'
+RED = '\033[91m'
+COLOR_RESET = '\033[0m'
 
-vypis_jmena("Rebyska", "Vydra")
-vypis_jmena("Miska", "Panda")
-vypis_jmena("Andrej", "Babis")
+animal_list = ["vydra", "tucnak", "ptakopysk", "panda", "koala", "peso"]
 
-jmeno = "Malicka"
-prijmeni = "Rebyska"
+animal = random.choice(animal_list)
 
-vypis_jmena(jmeno, prijmeni)
+animal_guess = None
 
-a = "Tucnak"
-b = "Cisarsky"
+while animal_guess != animal:
+    animal_guess = input("Na jake zvire myslim?: ")
+    if animal_guess != animal:
+        print(RED + "Spatne, nemyslim na: " + BLUE + animal_guess + COLOR_RESET)
+#       animal_list.remove(animal_guess)
+#       print("Zbyvajici moznosti jsou: " + BLUE + (COLOR_RESET + ", " + BLUE).join(animal_list) + COLOR_RESET)
 
-vypis_jmena(a, b)
-vyp
+print(GREEN + "Spravne, myslel jsem na: " + BLUE + animal + COLOR_RESET)
